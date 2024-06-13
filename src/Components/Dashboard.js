7313b177c862c87aebb6bd57c040a907343decfd
Prithivi Raj <prithiviraj.dk8055@gmail.com>
2024-06-13 08:40:52 +0530

    final commit

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -10,13 +10,13 @@ const Dashboard = ({
   handleshowpopup,
 }) => {
   return (
-    <div className="dashboard flex flex-row justify-center items-center font-  text-slate-50 w-full h-screen">
-      <div className="w-2/4 h-full flex justify-center items-center text-5xl">
-        Welcome to Tic Tac Toe
+    <div className="dashboard flex justify-center items-center  text-slate-50 w-full h-screen max-[300px]:flex-col">
+      <div className="w-2/4 h-full flex justify-center items-center flex-wrap text-5xl max-[300px]:text-2xl max-[300px]:w-full">
+        <span className="m-5 flex-wrap max-[300px]:m-2"> Welcome to Tic Tac Toe </span>
       </div>
-      <div className="w-2/4 h-full flex justify-center items-center flex-col">
+      <div className="w-2/4 h-full flex justify-center items-center flex-col max-[300px]:w-full">
         <button
-          className="m-4 text-3xl bg-slate-100 p-4 text-black rounded transition-all hover:text-4xl"
+          className="m-4 text-xl bg-slate-100 p-4 text-black rounded transition-all hover:text-2xl"
           onClick={() => {
             setDashData(!dashdata);
             setGameplay("player vs player");
@@ -25,7 +25,7 @@ const Dashboard = ({
           Play with Friend
         </button>
         <button
-          className="m-4 text-3xl bg-slate-100 p-4 text-black rounded transition-all hover:text-4xl"
+          className="m-4 text-xl bg-slate-100 p-4 text-black rounded transition-all hover:text-2xl"
           onClick={() => {
             // setDashData(!dashdata);
             setGameplay("player vs computer");
@@ -34,15 +34,18 @@ const Dashboard = ({
         >
           Play with Computer
         </button>
+        <div>
+        {showpopup && (
+          <DifficultyPopup
+            handleDifficulty={handleDifficulty}
+            handleshowpopup={handleshowpopup}
+            dashdata = {dashdata}
+            setDashData = {setDashData}
+          />
+        )}
+        </div>
       </div>
-      {showpopup && (
-        <DifficultyPopup
-          handleDifficulty={handleDifficulty}
-          handleshowpopup={handleshowpopup}
-          dashdata = {dashdata}
-          setDashData = {setDashData}
-        />
-      )}
+      
     </div>
   );
 };
--- src/Components/DifficultyPopup.js
+++ src/Components/DifficultyPopup.js
@@ -3,7 +3,7 @@ import { MdOutlineClose } from "react-icons/md";
 
 const DifficultyPopup = ({handleshowpopup,handleDifficulty,setDashData,dashdata}) => {
   return (
-    <div className='popupcontainer absolute w-[80%]  bottom-32 flex justify-evenly items-center h-28 '>
+    <div className='popupcontainer  w-[80%]  bottom-20 flex justify-evenly items-center h-28 '>
         <button className='m-4 text-3xl bg-slate-100 p-4 text-black rounded transition-all hover:text-4xl' onClick={(e) =>{ handleDifficulty(e); setDashData(!dashdata)}} >Easy </button>
         <button className='m-4 text-3xl bg-slate-100 p-4 text-black rounded transition-all hover:text-4xl' onClick={(e) =>{ handleDifficulty(e); setDashData(!dashdata)} } >Medium</button>
         <button className='m-4 text-3xl bg-slate-100 p-4 text-black rounded transition-all hover:text-4xl' onClick={(e) =>{ handleDifficulty(e); setDashData(!dashdata)}} >Hard</button>
